8f27d415c00e8f103297c703601ea1a0333aa073
khodi7 <42445258+khodi7@users.noreply.github.com>
2019-12-01 13:45:15 +0100

    Ajout de FrameList.py
    Le fichier FrameList.py contient deux classes : FrameList et Frame.
    Celles-ci sont utilisées pour créer une liste chainée.
    Cette dernière sera utilisée pour faire le menu.

---
+++ FrameList.py
@@ -0,0 +1,69 @@
+class FrameList:
+    """
+    Liste chainee de Frames.
+    """
+    def __init__(self):
+        """
+        pre : -
+        post : initialise un objet FrameList.
+        self.__lenght est le nombre de Frames que contient self.
+        self.__head est le premier Frame de self.
+        self.__tail_end est le dernier Frame de self.
+        """
+        self.__lenght = 0
+        self.__head = None
+        self.__tail_end = None
+        
+    def head(self):
+        return self.__head
+    
+    def tail_end(self):
+        return self.__tail_end
+        
+    def add_last(self, frame):
+        """
+        pre : frame est un Frame.
+        post : ajoute Frame a self.
+        Ce Frame sera reference par l'ancien Frame self.__tail_end et deviendra le nouveau self.__tail_end
+        """
+        #On attache frame au reste du LinkedList
+        self.tail_end().setnext(frame)
+        frame.setlast(self.tail_end())
+        self.__tail_end = frame
+        self.__lenght += 1
+    
+class Frame:
+    def __init__(self, cargo = None, nxt = None, last = None, up = None, down = None):
+        """
+        pre : -
+        post : initialise un objet Frame.
+        self.__cargo est le contenu du Frame, c'est ce qui est retourne par la methode __str__(self).
+        self.__last et self.__next peuvent etre des objets Frames.
+        self.__up et self.__down peuvent etre des objets FrameList.
+        """
+        self.__cargo = cargo
+        self.__last = last
+        self.__next = nxt
+        self.__up = up
+        self.__down = down
+        
+    def __str__(self):
+        return str(self.__cargo)
+    
+    def last(self):
+        return self.__last
+    
+    def next(self):
+        return self.__next
+    
+    def up(self):
+        return self.__up
+    
+    def down(self):
+        return self.__down
+    
+    def setnext(self, nxt):
+        self.__next = nxt
+        
+    def setlast(self, last):
+        self.__last = last
