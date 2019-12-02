92d256854c474fa86a142644b2afdf399190993b
khodi7 <42445258+khodi7@users.noreply.github.com>
2019-12-02 18:13:51 +0100

    Menu

--- demo.py
+++ demo.py
@@ -66,27 +66,51 @@ def register_message(content):
     with open("message.txt", "w") as file:
         file.writelines(content)
         
-def handle_wait_for_event(event):
+def handle_wait_for_event(event, frame):
     """
     pre : event est un objet SenseHat.stick.
+    frame est un objet Frame.
     post : retourne le nouveau Frame a afficher.
     Ne gere pour le moment que les mouvements gauche droite du joistick.
     """
     if event.action != "released":
         if event.direction == "right":
-            return current_frame.next()
+            return frame.next()
         if event.direction == "left":
-            return current_frame.last()
+            return frame.last()
         if event.direction == "down" and current_frame.down == None:
             call("sudo shutdown -h now", shell=True)    #TODO : ajouter une demande de confirmation
-        
+        if event.direction == "up" and current_frame.down != None:
+            return frame.up().head()
+        if event.direction == "down" and current_frame.down != None:
+            return frame.down()
 if __name__ == "__main__":
     #Creation des FrameLists qui composent les menus (a remplacer plus tard)
     main_menu = FrameList()
-    (a, b) = (Frame(cargo = "Lire le message", up = "display_msg"),\
-              Frame(cargo = "Nouveau message", up = "register_message"))
-    main_menu.add_last(a)
-    main_menu.add_last(b)
+    new_message_menu = FrameList()
+    keyboard_menu = FrameList()
+    keyboard_menu_frames = []
+    for i in range(10):
+        frame = Frame(cargo = i)    #TODO : define a up attribute here
+        keyboard_menu_frames.append(frame)
+    (a1, a2, a3, a4) = (Frame(cargo = "Entrer un message", up = keyboard_menu), Frame(cargo = "Entrer le mot de passe"),\
+                        Frame(cargo = "Voir le message"), Frame(cargo = "Valider"))
+    new_message_menu_frames = [a1, a2, a3, a4]
+    (b, a) = (Frame(cargo = "Lire le message"),\
+              Frame(cargo = "Nouveau message", up = new_message_menu))
+    main_menu_frames = [a, b]
+    
+    keyboard_menu.setdown(a1)
+    new_message_menu.setdown(a)
+    
+    for frame in keyboard_menu_frames:
+        keyboard_menu.add_last(frame)
+        
+    for frame in new_message_menu_frames:
+        new_message_menu.add_last(frame)
+        
+    for frame in main_menu_frames:
+        main_menu.add_last(frame)
     #Creation du SenseHat
     s = SenseHatEncodator()
     #Programme principal
@@ -99,7 +123,7 @@ if __name__ == "__main__":
             s.show_message(text_string = str(current_frame), scroll_speed = 0.05)
         event = s.stick.wait_for_event()
         print(event)
-        rcf = handle_wait_for_event(event)
+        rcf = handle_wait_for_event(event, current_frame)
         if rcf == None:       #Si current_frame ne change pas, on ne reaffiche pas le meme Frame.
             message_showed = False
         else:
